690da6d9e76b6c806b56989e570e8a3c0f15d30f
Poliakova-Iryna <167613528+Poliakova-Iryna@users.noreply.github.com>
2024-09-29 19:40:03 +0300

    upd2

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,16 +1,20 @@
 import iziToast from "izitoast";
 import axios from "axios";
+import { appendImagesHits } from "./render-functions";
+
 
 const BASE_URL = "https://pixabay.com/api/";
 const API_KEY = "46090964-55e0ede1337bbc868df4332a0";
+export const PER_PAGE = 15
 
-export async function fetchImags(searchQuery) {
+export async function fetchImags(searchQuery, currentPage = 1) {
     const options = new URLSearchParams({
         q: searchQuery,
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: 'true',
-        per_page: 15
+        per_page: PER_PAGE,
+        page: currentPage,
     });
 
     const url = `${BASE_URL}?key=${API_KEY}&${options}`;
@@ -18,15 +22,20 @@ export async function fetchImags(searchQuery) {
     const { data: {hits, totalHits}, request: {status} } = await axios.get(url);
 
     if(status !== 200) {
-        throw new Error(
-            iziToast.error({
-                title: 'Error',
-                message: 'Something went wrong...',
-                position: 'topRight',
-            })
-        )
+        iziToast.error({
+            title: 'Error',
+            message: 'Something went wrong...',
+            position: 'topRight',
+        })
+    };
+
+    if(hits.length === 0) {
+        iziToast.error({
+            title: 'Error',
+            message: 'Something went wrong...',
+            position: 'topRight',
+        })
     };
 
     return {hits, totalHits};
 }
-
--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import { appendImagesHits } from "./js/render-functions";
-import { fetchImags } from "./js/pixabay-api";
+import { fetchImags, PER_PAGE } from "./js/pixabay-api";
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 import axios from "axios";
+
+
+let currentPage = 1;
+let searchQuery = null;
+let pages = 0;
 
 const refs = {
     form: document.querySelector('.js-form'),
@@ -14,7 +19,8 @@ const refs = {
     gallery: document.querySelector('.js-gallery'),
 }; 
 
-refs.form.addEventListener('submit', handleSubmit) 
+refs.form.addEventListener('submit', handleSubmit);
+refs.loadMoreBtn.addEventListener('click', handleLoadMore);
 
 
 async function handleSubmit(event) {
@@ -22,31 +28,67 @@ async function handleSubmit(event) {
 
     const form = event.currentTarget;
 
-    const { value: searchQuery } = form.elements.query;
+    searchQuery  = form.elements.query.value;
+    refs.gallery.innerHTML = '';
+    currentPage = 1;
 
     try {
-        const { hits, totalHits } = await fetchImags(searchQuery);
-
-        refs.gallery.innerHTML = '';
-
+        const { hits, totalHits } = await fetchImags(searchQuery, currentPage);
+        pages = Math.ceil(totalHits / PER_PAGE);
         appendImagesHits(hits, refs.gallery);
-        refs.loadMoreBtn.classList.remove('is-hidden');
+        if(totalHits > PER_PAGE) {
+            refs.loadMoreBtn.classList.remove('is-hidden');
+        }
+        lightbox = new SimpleLightbox('.js-gallery a', { 
+            captionsData: 'alt', 
+            captionDelay: 250 
+        });
+    }
+    catch (error) {}
 
+    finally {
         form.reset();
-        gallery.refresh();
-
-    }
-    catch (error) {
-        iziToast.error({
-            title: 'Error',
-            message: 'Something went wrong',
-            position: 'topRight',
-        })
-    }
-    finally {}  
+    }  
 
 }
 
 
+async function handleLoadMore() {
+    currentPage += 1; 
+    try {
+        refs.loadMoreBtn.classList.remove('is-hidden');
+
+        const { hits } = await fetchImags(searchQuery, currentPage); 
+        appendImagesHits(hits, refs.gallery);
+        handleScrollView();
+
+        lightbox.refresh();
+        
+        if (currentPage >= Math.min(pages, 34)) {
+            refs.loadMoreBtn.classList.add('is-hidden');
+            iziToast.error({
+                title: 'Error',
+                message: 'We are sorry, but you have reached the end of search results.',
+                position: 'topRight',
+            })
+        }
+    } catch (error) {
+        console.error(error);
+    } finally {
+        
+    }
+}
+
+function handleScrollView() {
+    const lastHit = refs.container.lastElementChild;
+    const hitHeight = lastHit.getBoundingClientRect().height;
+    const scrollHeight = hitHeight * 2;
+
+    window.scrollBy({
+        top: scrollHeight,
+        left: 0,
+        behavior: 'smooth',
+    });
+}
 
 
